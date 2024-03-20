6e7e1309c10073ba8372481a2f7f75b2337ed115
Ximmatilloxon <147364427+Ximmatilloxon@users.noreply.github.com>
2024-03-20 11:36:50 +0500

    Update problem5.cpp

--- problem5.cpp
+++ problem5.cpp
@@ -1,19 +1,20 @@
 #include "iostream"
 using namespace std;
 
-void sortArray(int arr[], int l, int r){
-    if(l < r){
-        int last = arr[r], i = l - 1;
-        for(int j = i; j < r - 1; j++){
-            
-        }
-    }
-}
-
 int main(){
-    int n, m = NULL;
+    int n, c, m;
     cin >> n;
     int arr[n];
-    for (int i = 0; i < n; ++i) {cin >> arr[i];}
-    sortArray(arr, 0, n-1);
+    for(int i = 0; i < n; ++i){
+        cin >> arr[i];
+    }
+    m = arr[n-1];
+    for(int i = n-2; i > -1; --i){
+        c = arr[i];
+        arr[i] = m;
+        if(c > m) m = c;
+    }
+    arr[n-1] = -1;
+    for(int i:arr)cout << i << " ";
+
 }
