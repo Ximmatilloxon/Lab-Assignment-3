6c9dfb525fbabc8d51a4ec36df2619409a33af92
Ximmatilloxon <147364427+Ximmatilloxon@users.noreply.github.com>
2024-03-20 12:06:33 +0500

    Update problem6.cpp

--- problem6.cpp
+++ problem6.cpp
@@ -1,3 +1,19 @@
-//
-// Created by Lenov on 3/13/2024.
-//
+#include "iostream"
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; ++i) cin >> arr[i];
+    int x, y, d = n - 1;
+    cin >> x >> y;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if(arr[i] == x && arr[j] == y){
+                if(abs(j-i) < d) d = abs(j-i);
+            }
+        }
+    }
+    cout << d;
+}
